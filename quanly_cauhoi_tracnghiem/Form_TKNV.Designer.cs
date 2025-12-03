38a7c58681d1eb5eb1c0979ead28784ed16aafc5
Ninh <leducninh29031996@gmail.com>
2025-12-03 18:45:21 +0700

    Initial commit - Base project

---
+++ quanly_cauhoi_tracnghiem/Form_TKNV.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Tracnghiem_Application
+{
+    partial class Form_TKNV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.skinEngine1 = new Sunisoft.IrisSkin.SkinEngine(((System.ComponentModel.Component)(this)));
+            this.SuspendLayout();
+            // 
+            // skinEngine1
+            // 
+            this.skinEngine1.SerialNumber = "";
+            this.skinEngine1.SkinFile = null;
+            // 
+            // Form_TKNV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 264);
+            this.ControlBox = false;
+            this.DoubleBuffered = true;
+            this.EnableGlass = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form_TKNV";
+            this.Text = "Tìm kiếm nhân viên";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Sunisoft.IrisSkin.SkinEngine skinEngine1;
+    }
+}
---
+++ quanly_cauhoi_tracnghiem/Form_TKTS.cs
@@ -0,0 +1,19 @@
+using DevComponents.DotNetBar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tracnghiem_Application
+{
+    public partial class Form_TKTS : Office2007Form
+    {
+        public Form_TKTS()
+        {
+            InitializeComponent();
+        }
+    }
+}
